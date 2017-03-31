6fd57c706ad00a590f48d4943ece3fd1de458cb7
deancosanella <deancosanella@yahoo.com>
2017-03-30 23:02:39 -0700

    Added second page and button. Also added functionality.

--- app/src/main/java/com/tip/df/tip/CalculateActivity.java
+++ app/src/main/java/com/tip/df/tip/CalculateActivity.java
@@ -1,13 +1,64 @@
 package com.tip.df.tip;
 
+import android.content.Intent;
+import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.R.id.input;
 
 public class CalculateActivity extends AppCompatActivity {
+
+    TextView textViewTip15;
+    TextView textViewTip18;
+    TextView textViewTip20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate);
+
+        //Intent intent = getIntent();
+        Bundle bundle = getIntent().getExtras();
+
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textViewTip15 = (TextView) findViewById(R.id.textViewTip15);
+        textViewTip18 = (TextView) findViewById(R.id.textViewTip18);
+        textViewTip20 = (TextView) findViewById(R.id.textViewTip20);
+
+        Button backBtn = (Button) findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(CalculateActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        if(!bundle.isEmpty()) {
+            double bill = bundle.getDouble("key");
+            calculateTipAmount(bill);
+            textView.setText("Bill Amount: $" + String.format("%.2f", bill));
+        }
+        else {
+            System.out.println("bundle is empty.");
+        }
+    }
+
+    public void calculateTipAmount(double bill) {
+        double lowTip = bill * 0.15;
+        double medTip = bill * 0.18;
+        double highTip = bill * 0.2;
+
+        displayTipAmount(lowTip, medTip, highTip);
+    }
+
+    public void displayTipAmount(double l, double m, double h) {
+        textViewTip15.setText("15% tip:\t\t\t$" + String.format("%.2f", l));
+        textViewTip18.setText("18% tip:\t\t\t$" + String.format("%.2f", m));
+        textViewTip20.setText("20% tip:\t\t\t$" + String.format("%.2f", h));
+
     }
 }
--- app/src/main/java/com/tip/df/tip/MainActivity.java
+++ app/src/main/java/com/tip/df/tip/MainActivity.java
@@ -3,8 +3,12 @@ package com.tip.df.tip;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText input = (EditText)  findViewById(R.id.editText);
+
+
+        final Bundle bundle = new Bundle();
+
+
         Button button = (Button) findViewById(R.id.calculateBtn);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                double value = Double.parseDouble(input.getText().toString());
                 Intent intent = new Intent(MainActivity.this, CalculateActivity.class);
+                intent.putExtra("key", value);
                 startActivity(intent);
             }
         });
